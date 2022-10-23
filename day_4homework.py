56a9a82b17bd3c42e4e90d5f09e8f00cbc0aeda9
My Name <myemail@example.com>
2022-10-23 17:09:44 +0600

    exam test

---
+++ day_4homework.py
@@ -0,0 +1,32 @@
+bangla_1stPaper = 70
+bangla_2ndPaper = 90
+
+#bangla_1stPaper
+if bangla_1stPaper <= 32 or bangla_2ndPaper <= 32: 
+    print("F")
+elif bangla_1stPaper <=39:
+    print("D")
+elif bangla_1stPaper <= 49:
+    print("C")
+elif bangla_1stPaper <= 59:
+    print("B")
+elif bangla_1stPaper <= 69:
+    print("-A")
+elif bangla_1stPaper <= 79:
+    print("A")
+elif bangla_1stPaper <= 100:
+    print("A+")
+
+#bangla_2ndPaper
+if bangla_2ndPaper <=39:
+    print("D")
+elif bangla_2ndPaper <= 49:
+    print("C")
+elif bangla_2ndPaper <= 59:
+    print("B")
+elif bangla_2ndPaper <= 69:
+    print("-A")
+elif bangla_2ndPaper <= 79:
+    print("A")
+elif bangla_2ndPaper <= 100:
+    print("A+")
