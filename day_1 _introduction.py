d88075f7673e1120afa9b5679ad99e236ddd9c5e
My Name <myemail@example.com>
2022-11-02 00:59:04 +0600

    New

--- day_1 _introduction.py
+++ day_1 _introduction.py
@@ -1,1 +1,4 @@
 print("hello World")
+
+# First problem
+
--- day_7_screpSelenium.py
+++ day_7_screpSelenium.py
@@ -2,3 +2,5 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
+
+Practical XPath for Web Scraping link: https://www.scrapingbee.com/blog/practical-xpath-for-web-scraping/?fbclid=IwAR2goVl21Vo724XVwZc0uHJrbe_tLMgc0OMilalwEp8XsLMY5SgVrdN46GE
