acf031aee54273c273641ce69bdc1563857ba038
MdAhnafArshad <ahnafarshad.me@gmail.com>
2022-11-07 01:36:42 +0600

    nested loop

---
+++ day9/nestedLoop.py
@@ -0,0 +1,15 @@
+
+
+for i in range(10, 20):
+
+#same line print 
+    print(i, end=" ")
+    if(i == 19):
+     i = 20
+     print(i )
+
+
+
+for i in range(10):
+    for j in range(1, 5):
+        print(i,j)
--- day9/pagenation.py
+++ day9/pagenation.py
@@ -1,11 +1,22 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 
-options = Options()
-options.headless = True
-options.add_argument("--window-size=1920,1200")
-driver = webdriver.Chrome(options=options, service=Service(ChromeDriverManager().install()))
-driver.get("https://www.amazon.com/Dyson-V10-Allergy-Cordless-Cleaner/dp/B095LD5SWQ/")
+
+driver = webdriver.Chrome( service=Service(ChromeDriverManager().install()))
+driver.get("https://www.rokomari.com/electronics?ref=nm")
+
+title_list = []
+immage_list = []
+
+for section in range(1, 10):
+    title = driver.find_element(by=By.XPATH, value= '/html/body/div[5]/div/div[1]/div[2]/section/div[2]/div/div['+str(section)+']/div/a/div[2]/p[1]').text
+
+title_list.append(title)
+
+print(title_list)
+
+
+
+
