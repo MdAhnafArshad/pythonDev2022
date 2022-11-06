b2df61a4579aa32794e546eaabd9f1df5dbac815
MdAhnafArshad <ahnafarshad.me@gmail.com>
2022-11-07 02:18:18 +0600

    add list with xpath data collection

--- day9/pagenation.py
+++ day9/pagenation.py
@@ -5,17 +5,23 @@ from selenium.webdriver.common.by import By
 
 
 driver = webdriver.Chrome( service=Service(ChromeDriverManager().install()))
-driver.get("https://www.rokomari.com/electronics?ref=nm")
+driver.get("https://www.rokomari.com/product/category/2236/calculator")
 
 title_list = []
-immage_list = []
+img_list = []
 
-for section in range(1, 10):
-    title = driver.find_element(by=By.XPATH, value= '/html/body/div[5]/div/div[1]/div[2]/section/div[2]/div/div['+str(section)+']/div/a/div[2]/p[1]').text
+for section in range(1,6):
+        title = driver.find_element(by=By.XPATH, value= '/html/body/div[5]/div/div[1]/div[2]/section/div[2]/div/div['+str(section)+']/div/a/div[2]/p[1]').text
+        title_list.append(title)
+        
+        img = driver.find_element(by=By.XPATH, value= '/html/body/div[5]/div/div[1]/div[2]/section/div[2]/div/div['+str(section)+']/div/a/div[1]/img').get_attribute('src')
+        img_list.append(img)
+        
 
-title_list.append(title)
+
 
 print(title_list)
+print(img_list)
 
 
 
