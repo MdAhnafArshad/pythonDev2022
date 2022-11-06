2cc541d372033abddb14acc193a96d86657c03f6
MdAhnafArshad <ahnafarshad.me@gmail.com>
2022-11-06 13:28:06 +0600

    file add

---
+++ day9/pagenation.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+
+options = Options()
+options.headless = True
+options.add_argument("--window-size=1920,1200")
+driver = webdriver.Chrome(options=options, service=Service(ChromeDriverManager().install()))
+driver.get("https://www.amazon.com/Dyson-V10-Allergy-Cordless-Cleaner/dp/B095LD5SWQ/")
