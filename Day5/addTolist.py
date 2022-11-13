370630fc1065ed5015e316c5c87ef361281d9be8
MdAhnafArshad <ahnafarshad.me@gmail.com>
2022-11-13 16:43:41 +0600

    add to even or odd number on the list

---
+++ Day5/addTolist.py
@@ -0,0 +1,15 @@
+
+evenList = []
+oddList = []
+length = 20
+
+for i in range(length):
+    if(i%2==0):
+        evenList.append(i)
+    else:
+        oddList.append(i)
+    
+
+
+print("This list cary on the  even Number: ", evenList)
+print("This list cary on the  odd  Number: ", oddList)
